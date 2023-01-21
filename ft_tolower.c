3f96cea0cbd072d049474dcf5a7e94fe8fa866f6
Ruggero Dolzi <rdolzi@e3r10p8.42roma.it>
2023-01-21 16:39:49 +0100

    Fix function name

--- ft_tolower.c
+++ ft_tolower.c
@@ -6,12 +6,12 @@
 /*   By: rdolzi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 15:43:29 by rdolzi            #+#    #+#             */
-/*   Updated: 2023/01/21 12:56:33 by rdolzi           ###   ########.fr       */
+/*   Updated: 2023/01/21 16:32:18 by rdolzi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-int	ft_toupper(int c)
+int	ft_tolower(int c)
 {
 	if (c >= 65 && c <= 90)
 		return (c + 32);
